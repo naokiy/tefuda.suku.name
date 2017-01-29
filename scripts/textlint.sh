1f07da677391cd2d3cbd5291e0b8c2f95b6a005b
naokiy <to_contact@naokiy.net>
2017-01-29 13:16:40 +0000

    add saddler

--- scripts/textlint.sh
+++ scripts/textlint.sh
@@ -11,9 +11,19 @@ gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
 
 diffBranchName="origin/develop"
 # 変更行のみを対象にする
+
+DIFF_FILE_COUNT=$(git diff --name-only --diff-filter=ACMR ${diffBranchName} \
+| grep -a '_posts/.*.md$' \
+| wc -l)
+
+if [ ${DIFF_FILE_COUNT} -eq 0 ]; then
+  echo 'no source diff'
+  exit 0
+fi
+
 git diff --name-only --diff-filter=ACMR ${diffBranchName} \
 | grep -a '_posts/.*.md$' \
-| xargs -r $(npm bin)/textlint -f checkstyle \
+| xargs $(npm bin)/textlint -f checkstyle \
 | checkstyle_filter-git diff ${diffBranchName} \
 | saddler report \
     --require saddler/reporter/github \
