53cfead6a369dccb6174a742b851f1aaa41b57c0
naokiy <to_contact@naokiy.net>
2017-01-29 12:39:19 +0000

    add saddler

--- scripts/textlint.sh
+++ scripts/textlint.sh
@@ -5,8 +5,14 @@ if [ "${CI_PULL_REQUEST}" == false ] || [ -z "$CI_PULL_REQUEST" ]; then
   exit 0
 fi
 
-diffBranchName="origin/develop"
+gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
 
+diffBranchName="origin/develop"
+# 変更行のみを対象にする
 git diff --name-only --diff-filter=ACMR ${diffBranchName} \
 | grep -a '_posts/.*.md$' \
-| xargs $(npm bin)/textlint
+| xargs $(npm bin)/textlint -f checkstyle \
+| checkstyle_filter-git diff ${diffBranchName} \
+| saddler report \
+    --require saddler/reporter/github \
+    --reporter Saddler::Reporter::Github::PullRequestReviewComment
